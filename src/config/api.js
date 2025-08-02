3de1650aa66e0d072026b756a3cf6332755f4e1c
Nabuhotnii Roman <91484253+NabuhotniiRoman@users.noreply.github.com>
2025-08-02 15:06:17 +0000

    update API_BASE_URL

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
 // API Configuration
-const API_BASE_URL = 'http://10.244.0.117:8080';  // Завжди використовуємо повний URL для API
+const API_BASE_URL = 'http://192.168.49.2:30080';  // Використовуємо NodePort для доступу до API
 
 export default API_BASE_URL;
