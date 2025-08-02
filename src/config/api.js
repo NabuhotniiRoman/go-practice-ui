438e6e0e18bd3972810611a22a4b14483f7c5212
Nabuhotnii Roman <91484253+NabuhotniiRoman@users.noreply.github.com>
2025-08-02 14:50:32 +0000

    update base url

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:8080';  // Завжди використовуємо localhost:8080 для port forwarding
+const API_BASE_URL = '10.244.0.117:8080';  // Завжди використовуємо localhost:8080 для port forwarding
 
 export default API_BASE_URL;
